69697192672822aae0a280e5837d4661291b9962
kevanultroneous <dev21.ultroneous@gmail.com>
2022-06-29 15:12:04 +0530

    jun 29 3:11 pm :  menubar , global , action button , actionbutton v2

--- components/Common/MenuBar.js
+++ components/Common/MenuBar.js
@@ -2,87 +2,13 @@ import styles from "@/styles/components/Sidebar.module.css";
 import { useEffect, useState } from "react";
 import { Col, Image, Row } from "react-bootstrap";
 import AOS from "aos";
+import { iconsMenu, menus } from "utils/menu.data";
 const MenuBar = ({ display = "none" }) => {
   useEffect(() => {
     AOS.refresh();
     AOS.init();
   }, []);
-  const menus = [
-    {
-      name: "Home",
-      menus: [
-        "Company Overview",
-        "Case study",
-        "Career page",
-        "Events & webinars",
-        "Awards & Certificate",
-        "Client Speak/Customer",
-      ],
-    },
-    {
-      name: "About us",
-      menus: [
-        "Career page",
-        "Events & webinars",
-        "Awards & Certificate",
-        "Client Speak/Customer",
-      ],
-    },
-    {
-      name: "Insights",
-      menus: ["Company Overview", "Case study", "Client Speak/Customer"],
-    },
-    {
-      name: "Resources",
-      menus: ["Company Overview", "Case study", "Career page"],
-    },
-    {
-      name: "Contact us",
-      menus: ["Company Overview", "Case study"],
-    },
-  ];
-  const iconsMenu = [
-    {
-      icon: "/assets/images/menu-1.svg",
-      color: "#F27E63",
-      name: "MxTrade",
-    },
-    {
-      icon: "/assets/images/menu-2.svg",
-      color: "#00ACD7",
-      name: "MxFuture",
-    },
-    {
-      icon: "/assets/images/menu-3.svg",
-      color: "#F2AB27",
-      name: "MxDex",
-    },
-    {
-      icon: "/assets/images/menu-4.svg",
-      color: "#4BA6A6",
-      name: "MxFi",
-    },
-    {
-      icon: "/assets/images/menu-5.svg",
-      color: "#9E9BF2",
-      name: "MxLaunch",
-    },
-    {
-      icon: "/assets/images/menu-6.svg",
-      color: "#BF9A78",
-      name: "MxWallet",
-    },
-    {
-      icon: "/assets/images/menu-7.svg",
-      color: "#4B99FF",
-      name: "MxBlock",
-    },
-    {
-      icon: "/assets/images/menu-8.svg",
-      color: "#D95276",
-      name: "MxNFT",
-    },
-  ];
+
   const [currentIcon, setCurrentIcon] = useState(null);
   const [currentMenu, setCurrentMenu] = useState(0);
   const Heading = ({ name, border, color }) => {
