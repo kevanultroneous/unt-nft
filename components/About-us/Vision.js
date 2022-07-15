04886ed5e259b36b81a3e589855fa1d3db9e85fb
Virag Shah <dev15.ultroneous@gmail.com>
2022-07-15 21:08:52 +0530

    Gradiantchanges

--- components/About-us/Vision.js
+++ components/About-us/Vision.js
@@ -1,7 +1,10 @@
 import { Button, Col, Row } from "react-bootstrap";
 import styles from "@/styles/components/aboutus/Vision.module.css";
+import { useState } from "react";
 
 const Vision = () => {
+  const [vision, setVision] = useState(true);
+
   return (
     <div className={styles.VisionContainer}>
       <div className={styles.Important}>
@@ -11,9 +14,27 @@ const Vision = () => {
           </Col>
           <Col xl={10}>
             <div className={styles.Heading}>
-              <h3 className={styles.Vision}>Vision</h3>
+              <h3
+                onClick={() => setVision(!vision)}
+                className={
+                  vision
+                    ? `${styles.Vision}  ${styles.Active} `
+                    : `${styles.Vision} ${styles.VisionHover}`
+                }
+              >
+                Vision
+              </h3>
               <span className={styles.span}>&</span>
-              <h3 className={styles.Mission}>Mission</h3>
+              <h3
+                onClick={() => setVision(!vision)}
+                className={
+                  !vision
+                    ? `${styles.Mission} ${styles.Active} `
+                    : `${styles.Mission} ${styles.VisionHover}`
+                }
+              >
+                Mission
+              </h3>
             </div>
           </Col>
         </Row>
@@ -21,13 +42,19 @@ const Vision = () => {
           <Col xl={2}></Col>
           <Col xl={10} className={styles.Detailcol}>
             <div className={styles.Detail}>
-              <p className={styles.Description}>
-                We have a vision to consistently provide innovative real-world
-                solutions to a global clientele that enhance customer
-                experiences and describe the future today. Our group of creative
-                and collaborative people solves current problems faced by
-                customers in a way that simplifies their business.
-              </p>
+              {vision ? (
+                <p className={styles.Description}>
+                  We have a vision to consistently provide innovative real-world
+                  solutions to a global clientele that enhance customer
+                  experiences and describe the future today. Our group of
+                  creative and collaborative people solves current problems
+                  faced by customers in a way that simplifies their business.
+                </p>
+              ) : (
+                <p>
+                  <p className={styles.Description}> This is changed content</p>
+                </p>
+              )}
             </div>
           </Col>
         </Row>
--- components/Products/NextProduct.js
+++ components/Products/NextProduct.js
@@ -3,32 +3,43 @@ import { Col, Row } from "react-bootstrap";
 import WaterText from "../Common/WaterText";
 const NextProduct = ({ listofdata = [] }) => {
   return (
-    <Row
-      className={`${styles.NextProductContainer} m-0 p-0 justify-content-center`}
-    >
-      {listofdata.map((value, index) => (
-        <Col
-          xl={3}
-          xs={12}
-          key={index}
-          className={`${styles.SpaceForMob} ${
-            index === 0
-              ? "text-xl-end text-xs-start"
-              : index === 1
-              ? "text-center"
-              : null
-          }`}
-        >
-          <WaterText color={value.color} text={value.name} />
-          {/* <div className={styles.Outer}>
-            <div className={styles.Bg}>
-              <div className={styles.Inner}>
-                <h1>GRADIENT TEXT ON HOVER</h1>
-              </div>
+    // <Row
+    //   className={`${styles.NextProductContainer} m-0 p-0 justify-content-center`}
+    // >
+    //   {listofdata.map((value, index) => (
+    //     <Col
+    //       xl={4}
+    //       xs={12}
+    //       key={index}
+    //       className={`${styles.SpaceForMob} ${
+    //         index === 0
+    //           ? "text-xl-end text-xs-start"
+    //           : index === 1
+    //           ? "text-center"
+    //           : null
+    //       }`}
+    //     >
+    //       <WaterText color={value.color} text={value.name} />
+    //       {/* <div className={styles.Outer}>
+    //         <div className={styles.Bg}>
+    //           <div className={styles.Inner}>
+    //             <h1>GRADIENT TEXT ON HOVER</h1>
+    //           </div>
+    //         </div>
+    //       </div> */}
+    //     </Col>
+    //   ))}
+    // </Row>
+    <Row>
+      <Col xs={12} xl={4} className={styles.Col}>
+        <div class={styles.outer}>
+          <div class={styles.bg}>
+            <div class={styles.inner}>
+              <h1 className={styles.h1}> GRADIENT TEXT ON HOVER</h1>
             </div>
-          </div> */}
-        </Col>
-      ))}
+          </div>
+        </div>
+      </Col>
     </Row>
   );
 };
