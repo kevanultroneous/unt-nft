9edfd962abcfac4938ef63e2a7e2b3c7153d4c7e
kevanultroneous <dev21.ultroneous@gmail.com>
2022-08-08 21:40:16 +0530

    parallax-completed

--- components/Home/Parallax.js
+++ components/Home/Parallax.js
@@ -1,9 +1,11 @@
 import styles from "@/styles/components/home/Parallax.module.css";
 import { Col, Row } from "react-bootstrap";
-const Parallax = () => {
+import { Parallax } from "react-parallax";
+
+const ParallaxComponent = () => {
   return (
     <div>
-      <section className={styles.Mainsection}>
+      {/* <section className={styles.Mainsection}>
         <div className={styles.Parallax}>
           <div className={styles.Effect}>
             <Row className={styles.Parallaxrow}>
@@ -34,14 +36,47 @@ const Parallax = () => {
             </Row>
           </div>
         </div>
-      </section>
+      </section> */}
       {/* <div className={styles.wrap}>
                 <Col xl={10}>
                   <h3 className={styles.MainHeading}>SCALABLE.</h3>
                 </Col>
               </div> */}
+
+      <Parallax bgImage={"/assets/images/Parallaxnew1.png"} strength={600}>
+        <div style={{ height: 800 }}>
+          <div className={styles.Effect}>
+            <Row className={styles.Parallaxrow}>
+              <Col xl={10}>
+                <p className={styles.Pera}>10+ Million Transaction Processed</p>
+              </Col>
+              <Col xl={10}>
+                <h3 className={styles.MainHeading}>SCALABLE.</h3>
+              </Col>
+            </Row>
+            <Row className={styles.Parallaxrow}>
+              <Col xl={10}>
+                <p className={styles.Pera}>32+ Global Blockchain Customers</p>
+              </Col>
+              <Col xl={10}>
+                <h3 className={styles.MainHeadingSecure}>Secure.</h3>
+              </Col>
+            </Row>
+            <Row className={styles.Parallaxrow}>
+              <Col xl={10}>
+                <p className={styles.Pera}>
+                  7+ Years of Unique Blockchain Experience
+                </p>
+              </Col>
+              <Col xl={10}>
+                <h3 className={styles.MainHeadingSuccess}>Successive.</h3>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      </Parallax>
     </div>
   );
 };
 
-export default Parallax;
+export default ParallaxComponent;
