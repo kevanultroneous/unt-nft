b3be3b9692ff936a74dd79d9ee5b5ef16705fd01
Virag Shah <dev15.ultroneous@gmail.com>
2022-08-10 18:32:52 +0530

    Mission alignment change,menu iconabout imgae

--- components/About-us/Hero.js
+++ components/About-us/Hero.js
@@ -85,7 +85,7 @@ const Hero = ({ clickHandler }) => {
           <Col xl={10} className={styles.Aboutcol}>
             <div className={styles.Heroimg}>
               <Image
-                src={"/assets/images/AboutTimeline.png"}
+                src={"/assets/images/AboutTimeline-1.jpg"}
                 alt="aboutheroimg"
                 className={styles.Img}
                 data-aos="fade-left"
