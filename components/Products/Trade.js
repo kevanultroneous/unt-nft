055ec34ccf275cd0805d568f058a700a75fff437
Virag Shah <dev15.ultroneous@gmail.com>
2022-08-12 17:59:47 +0530

    Trade section changes,vertical teaxt changes

--- components/Products/Trade.js
+++ components/Products/Trade.js
@@ -1,49 +1,48 @@
 import { Col, Row } from "react-bootstrap";
-import styles from "@/styles/components/Products/Trade.module.css"
+import styles from "@/styles/components/Products/Trade.module.css";
 import { HiOutlineExternalLink } from "react-icons/hi";
-export default function Trade({ firstcolor = "#363637", secondcolor = "#79797a", thirdcolor = "#bcbcbc" }) {
-    return (
-        <Row className={styles.NextRow}>
-            <Col xl={12} className={styles.ColOne}
-            >
-                <div className={styles.TradeHeadingWrraper}>
-                    <div className={styles.outer}>
-                        <div className={styles.bg}
-                            style={{
-                                background: `-moz-linear-gradient(90deg, ${firstcolor} 0%, ${secondcolor} 50%, ${thirdcolor} 100%)`,
-                                background: `-webkit-gradient(linear,90deg top,right top,
+export default function Trade({
+  firstcolor = "#363637",
+  secondcolor = "#79797a",
+  thirdcolor = "#bcbcbc",
+}) {
+  return (
+    <Row className={styles.NextRow}>
+      <Col xl={12} className={styles.ColOne}>
+        <div className={styles.TradeHeadingWrraper}>
+          <div className={styles.outer}>
+            <div
+              className={styles.bg}
+              style={{
+                background: `-moz-linear-gradient(90deg, ${firstcolor} 0%, ${secondcolor} 50%, ${thirdcolor} 100%)`,
+                background: `-webkit-gradient(linear,90deg top,right top,
                                 color-stop(0%, ${firstcolor}),
                                 color-stop(50%, ${secondcolor}),
                                 color-stop(100%, ${thirdcolor}))`,
-                                background: `-webkit-linear-gradient(90deg, ${firstcolor} 0%, ${secondcolor} 50%, ${thirdcolor} 100%)`,
-                                background: `-o-linear-gradient(90deg, ${firstcolor} 0 %, ${secondcolor} 50 %, ${thirdcolor} 100 %)`,
-                                background: `-ms-linear-gradient(90deg, ${firstcolor} 0 %, ${secondcolor} 50 %, ${thirdcolor} 100 %)`,
-                                background: `linear-gradient(90deg, ${firstcolor} 0%, ${secondcolor} 50%, ${thirdcolor} 100%)`,
-                            }}
-                        >
-                            <div className={styles.inner}>
-                                <h1 className={styles.h1}>Trade</h1>
-                            </div>
+                background: `-webkit-linear-gradient(90deg, ${firstcolor} 0%, ${secondcolor} 50%, ${thirdcolor} 100%)`,
+                background: `-o-linear-gradient(90deg, ${firstcolor} 0 %, ${secondcolor} 50 %, ${thirdcolor} 100 %)`,
+                background: `-ms-linear-gradient(90deg, ${firstcolor} 0 %, ${secondcolor} 50 %, ${thirdcolor} 100 %)`,
+                background: `linear-gradient(90deg, ${firstcolor} 0%, ${secondcolor} 50%, ${thirdcolor} 100%)`,
+              }}
+            >
+              <div className={styles.inner}>
+                <h1 className={styles.h1}>Trade</h1>
+              </div>
+            </div>
+          </div>
+          <span>
+            <HiOutlineExternalLink
+              className={styles.ContactusIcon}
+              style={{ color: secondcolor }}
+            />
+          </span>
+        </div>
 
-                        </div>
-
-                    </div>
-                    <span>
-                        <HiOutlineExternalLink
-                            className={styles.ContactusIcon}
-                            style={{ color: firstcolor }}
-                        />
-                    </span>
-                </div>
-
-                <p
-                    className={styles.ExchnageParagraph}
-                    style={{ color: firstcolor }}
-                >
-                    Get a free demo to dive in the crypto world with a smart exchange
-                    platform.
-                </p>
-            </Col>
-        </Row>
-    )
+        <p className={styles.ExchnageParagraph} style={{ color: firstcolor }}>
+          Get a free demo to dive in the crypto world with a smart exchange
+          platform.
+        </p>
+      </Col>
+    </Row>
+  );
 }
