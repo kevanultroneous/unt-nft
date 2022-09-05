51f89969cdf1f4bba4807c2af490868e5487c60b
Virag Shah <dev15.ultroneous@gmail.com>
2022-09-05 10:19:48 +0530

    resolvecursor animation

--- pages/_app.js
+++ pages/_app.js
@@ -19,17 +19,18 @@ function MyApp({ Component, pageProps }) {
           innerScale={0.1}
           outerScale={2}
           clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
+            "none",
+            // "a",
+            // 'input[type="text"]',
+            // 'input[type="email"]',
+            // 'input[type="number"]',
+            // 'input[type="submit"]',
+            // 'input[type="image"]',
+            // "label[for]",
+            // "select",
+            // "textarea",
+            // "button",
+            // ".link",
           ]}
         />
       ) : null}
