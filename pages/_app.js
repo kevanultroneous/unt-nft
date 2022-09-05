2bba4e000a9d11e3d3e57010fc50596d50f6bbae
Virag Shah <dev15.ultroneous@gmail.com>
2022-09-05 10:18:24 +0530

    cursor changes

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,19 @@ function MyApp({ Component, pageProps }) {
           outerAlpha={0.1}
           innerScale={0.1}
           outerScale={2}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+          ]}
         />
       ) : null}
       <Component {...pageProps} />
